80d3c7fcf6ee7e0428d979a537791be34921d7ae
zNitche <53448802+zNitche@users.noreply.github.com>
2024-03-09 18:26:26 +0100

    app root styling implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import Router from "./router";
+import Navbar from "./components/navbar/navbar"
+import Router from "./router"
 
 function App() {
-    return <Router />
+    return (
+        <div className="main-wrapper">
+            <div className="header-wrapper">
+                piScan
+            </div>
+            <div className="content-wrapper">
+                <Router />
+            </div>
+            <Navbar />
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,26 @@
+import { useLocation } from 'wouter';
+import HomeIcon from '../../icons/home'
+import PrinterIcon from '../../icons/printer'
+import ScanIcon from '../../icons/scan'
+import classes from './navbar.module.css'
+
+export default function Navbar() {
+    const [_location, navigate] = useLocation();
+
+    return (
+        <div className={classes.wrapper}>
+            <div className={classes.icon} onClick={
+                () => {
+                    navigate('/')
+                }}>
+                <HomeIcon />
+            </div>
+            <div className={classes.icon}>
+                <PrinterIcon />
+            </div>
+            <div className={classes.icon}>
+                <ScanIcon />
+            </div>
+        </div>
+    )
+}
