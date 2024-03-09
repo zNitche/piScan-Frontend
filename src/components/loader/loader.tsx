b0cab0cad3c6b44c821e8813abffcb3aec27acc2
zNitche <53448802+zNitche@users.noreply.github.com>
2024-03-09 18:58:26 +0100

    added spiner loader

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,14 @@
+import clsx from '../../utils/clsx';
+import classes from './loader.module.css'
+
+interface LoaderProps {
+    variant?: "sm" | "md" | "xl";
+  }
+
+export default function Loader({variant}: LoaderProps) {
+    const variantClass = variant !== undefined ? classes[`size-${variant}`] : classes[`size-md`];
+
+    return (
+        <div className={clsx(classes.wrapper, variantClass)} />
+    )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import Loader from '../../components/loader/loader'
 import HomeIcon from '../../icons/home'
 import classes from './index.module.css'
 
@@ -5,6 +6,7 @@ export default function Home() {
     return (
         <div className={classes.wrapper}>
             <HomeIcon /> Hello Home
+            <Loader />
         </div>
     )
 }
