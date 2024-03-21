b96e78dbbe1f2cf3b4b2769ca6b8647744d9f599
zNitche <53448802+zNitche@users.noreply.github.com>
2024-03-21 23:51:19 +0100

    test styling refactoring tabs to spaces

--- src/components/add_device_card/add_device_card.tsx
+++ src/components/add_device_card/add_device_card.tsx
@@ -2,9 +2,9 @@ import AddIcon from "@/icons/add";
 import classes from "./add_device_card.module.css";
 
 export default function AddDeviceCard() {
-	return (
-		<div className={classes.wrapper}>
-			<AddIcon className={classes.icon} />
-		</div>
-	);
+    return (
+        <div className={classes.wrapper}>
+            <AddIcon className={classes.icon} />
+        </div>
+    );
 }
