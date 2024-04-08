0f895d6a7c597a32c6291501dde80139e4f33562
zNitche <53448802+zNitche@users.noreply.github.com>
2024-04-08 21:37:57 +0200

    file card tweaks

--- src/components/file_card/file_card.tsx
+++ src/components/file_card/file_card.tsx
@@ -3,12 +3,14 @@ import classes from "./file_card.module.css";
 import { useMemo } from "react";
 import { formatDateToNormal } from "@/utils/date";
 import ErrorIndicatorIcon from "@/icons/error_indicator";
+import DeleteIcon from "@/icons/delete";
 
 interface FileCardProps {
     file: ScanFile;
+    onRemoveIconClick?: () => void;
 }
 
-export default function FileCard({ file }: FileCardProps) {
+export default function FileCard({ file, onRemoveIconClick }: FileCardProps) {
     const createdAtDate = useMemo(() => {
         const date = formatDateToNormal(new Date(file.created_at));
         return date ? date : "---";
@@ -17,18 +19,35 @@ export default function FileCard({ file }: FileCardProps) {
     return (
         <div className={classes.wrapper}>
             <div className={classes.content}>
-                <div className={classes.name}>
-                    {file.name ? file.name : "---"}
+                <div className={classes.top}>
+                    <div className={classes.name}>
+                        {file.name ? file.name : "---"}
+                    </div>
+                    <div className={classes["top-actions"]}>
+                        <div
+                            onClick={() => {
+                                onRemoveIconClick?.();
+                            }}
+                        >
+                            <DeleteIcon />
+                        </div>
+                    </div>
                 </div>
                 <div className={classes["image-wrapper"]}>
-                    {file.image?.thumbnail_url ? (
-                        <img
-                            src={file.image.thumbnail_url}
-                            className={classes.image}
-                        />
-                    ) : (
-                        <ErrorIndicatorIcon />
-                    )}
+                    <a
+                        target="_blank"
+                        href={file.image.preview_url}
+                        rel="noreferrer"
+                    >
+                        {file.image?.thumbnail_url ? (
+                            <img
+                                src={file.image.thumbnail_url}
+                                className={classes.image}
+                            />
+                        ) : (
+                            <ErrorIndicatorIcon />
+                        )}
+                    </a>
                 </div>
                 <div className={classes.date}>{createdAtDate}</div>
             </div>
